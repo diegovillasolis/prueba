1ae7b4b4f1e9cf1ad5ddbd3f8b20b5e703f543c0
Diego Villarroel Solis <diego.villarroel@outlook.com>
2017-11-27 17:46:39 -0400

    resolve 404 errors

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,41 +1,36 @@
 import React from 'react';
 import { Select } from 'semantic-ui-react';
 import { Input } from 'semantic-ui-react';
+import { Dropdown } from 'semantic-ui-react';
 import { fetchWeather } from '../utils/api';
 
 class Search extends React.Component{
   constructor(props){
     super(props);
     this.handleChange = this.handleChange.bind(this);
+    this.cityOptions = [
+      { key: 'CB', value: 'Cochabamba', text: 'Cochabamba' },
+      { key: 'SB', value: 'Sacaba', text: 'Sacaba' },
+      { key: 'AR', value: 'Arani', text: 'Arani' },
+      { key: 'CR', value: 'Carrasco', text: 'Carrasco' },
+      { key: 'CP', value: 'Capinota', text: 'Capinota' },
+      { key: 'MZ', value: 'Mizque', text: 'Mizque' },
+      { key: 'PN', value: 'Punata', text: 'Punata' },
+    ];
+    this.state = {value: ""};
   }
 
-  handleChange(e){
-    console.log(e.target.value);
-    const newValue = e.target.value;
-    this.props.onChange(newValue);    
+  handleChange(e, { value }){
+    this.setState({
+      value
+    });
+    this.props.onChange(value);    
   }
   
   render(){
     return (
       <div className="Search">
-        <Input list='cities' placeholder='Choose a city...' onChange={this.handleChange} />
-        <datalist id='cities'>
-          <option value='Cochabamba' />
-          <option value='Sacaba' />
-          <option value='Quillacollo' />
-          <option value='Arani' />
-          <option value='Arque' />
-          <option value='Ayopaya' />
-          <option value='Bolivar' />
-          <option value='Carrasco' />
-          <option value='Capinota' />
-          <option value='Campero' />
-          <option value='Cercado' />
-          <option value='Chapare' />
-          <option value='Mizque' />
-          <option value='Punata' />
-          <option value='Tiraque' />
-        </datalist>
+        <Dropdown placeholder='City...' search selection options={this.cityOptions}  onChange={this.handleChange}  value={this.state.value} />
       </div>
     );    
   }
